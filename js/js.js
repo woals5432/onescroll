9af03e28e6064f4ac2a2a5225ab25f5f40ea7e93
woals5432 <woals5432@gmail.com>
2023-09-19 17:41:22 +0900

    첫업로드

---
+++ js/js.js
@@ -0,0 +1,24 @@
+// 스크롤이 되면 헤더 모양 바뀜
+const header = document.querySelector('header');
+
+
+window.addEventListener('scroll',function(){
+  if(window.scrollY > 500){
+    header.classList.add('on')
+  }
+  if(window.scrollY <= 500){
+    header.classList.remove('on')
+  }
+})
+
+//헤더 버튼 
+const menuItems = document.querySelectorAll('nav ul>li>a');
+
+menuItems.forEach(function(item,idx){
+  item.onclick = function(e){
+    e.preventDefault();
+    document.querySelector(`#s${idx+1}`).scrollIntoView({
+      behavior:'smooth'
+    })
+  }
+})
