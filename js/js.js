18b4ba179a4caa8d2a3b9faf5f5f653af537892f
woals5432 <woals5432@gmail.com>
2023-09-20 10:05:47 +0900

    1.1

--- js/js.js
+++ js/js.js
@@ -1,7 +1,6 @@
 // 스크롤이 되면 헤더 모양 바뀜
 const header = document.querySelector('header');
 
-
 window.addEventListener('scroll',function(){
   if(window.scrollY > 500){
     header.classList.add('on')
@@ -21,4 +20,19 @@ menuItems.forEach(function(item,idx){
       behavior:'smooth'
     })
   }
+})
+
+// 아래로 가기 버튼 .btnDown
+for(let i = 1;i < 6; i++){
+  document.querySelector(`#s${i} .btnDown`).onclick = function(){
+    document.querySelector(`#s${i+1}`).scrollIntoView({
+      behavior:'smooth'
+    })
+  }
+}
+
+//제일 하단의 top버튼
+const top1 = document.querySelector('.top');
+top1.addEventListener('click',()=>{
+  window.scroll({top:0,behavior:"smooth"})
 })
